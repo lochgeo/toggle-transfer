394cabd72c7de2d6ffab7eb6c31d1919e1af4ba7
lochgeo <lg1983@gmail.com>
2016-07-24 18:49:53 +0530

    remove receipient approval

--- sol/lochCoin.sol
+++ sol/lochCoin.sol
@@ -8,18 +8,15 @@ contract MyToken {
 
     /* This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
+
+    /* This creates an array with all allowances granted by token owners */
     mapping (address => mapping (address => uint256)) public allowance;
 
     /* This generates a public event on the blockchain that will notify clients */
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    event notifyTransfer(address indexed from, address indexed to, uint256 value);
 
     /* Initializes contract with initial supply tokens to the creator of the contract */
-    function MyToken(
-        uint256 initialSupply,
-        string tokenName,
-        uint8 decimalUnits,
-        string tokenSymbol
-        ) {
+    function MyToken(uint256 initialSupply, string tokenName, uint8 decimalUnits, string tokenSymbol) {
         balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
         totalSupply = initialSupply;                        // Update total supply
         name = tokenName;                                   // Set the name for display purposes
@@ -30,29 +27,38 @@ contract MyToken {
 
     /* Send coins */
     function transfer(address _to, uint256 _value) {
-        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+        if (balanceOf[msg.sender] < _value) 
+            throw;                                           // Check if the sender has enough
+        
+        if (balanceOf[_to] + _value < balanceOf[_to]) 
+            throw;                                           // Check for overflows
+        
         balanceOf[msg.sender] -= _value;                     // Subtract from the sender
         balanceOf[_to] += _value;                            // Add the same to the recipient
-        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+        notifyTransfer(msg.sender, _to, _value);             // Notify anyone listening that this transfer took place
     }
 
     /* Allow another contract to spend some tokens in your behalf */
-    function approveAndCall(address _spender, uint256 _value)
-        returns (bool success) {
+    function approveAndCall(address _spender, uint256 _value) returns (bool success) {
         allowance[msg.sender][_spender] = _value;
         return true;
     }
 
     /* A contract attempts to get the coins */
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (balanceOf[_from] < _value) throw;                 // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw;  // Check for overflows
-        if (_value > allowance[_from][msg.sender]) throw;   // Check allowance
+        if (balanceOf[_from] < _value) 
+            throw;                                           // Check if the sender has enough
+        
+        if (balanceOf[_to] + _value < balanceOf[_to]) 
+            throw;                                           // Check for overflows
+        
+        if (_value > allowance[_from][msg.sender]) 
+            throw;                                           // Check allowance
+        
         balanceOf[_from] -= _value;                          // Subtract from the sender
         balanceOf[_to] += _value;                            // Add the same to the recipient
-        allowance[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
+        allowance[_from][msg.sender] -= _value;              // Reduce the Allowance
+        notifyTransfer(_from, _to, _value);
         return true;
     }
 
